183c42b559c8aebb625db5c00f9ec05e885c6139
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-09 13:34:35 -0400

    commit tree deee19957d17ea95d69e7df161a93f21c94112d0

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 5) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(3, 4, 5, 6, 7, 8, 9, 20);
